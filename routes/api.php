4eae5af0b349be0a48606f0c9a524cb1222e40ec
Yvesthior <yvesthior@gmail.com>
2021-09-13 10:40:48 +0000

    added auth for api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
     // Analyses
     Route::apiResource('analysis', 'AnalysesApiController');
 });
+
+
+Route::post('register', 'Api\\AuthController@register');
+Route::post('login', 'Api\\AuthController@login');
