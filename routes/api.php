c610318acf193222f214527869e40b9d7cfbefb1
Yvesthior <yvesthior@gmail.com>
2021-09-13 10:42:27 +0000

    test commit

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,5 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
 
 Route::post('register', 'Api\\AuthController@register');
 Route::post('login', 'Api\\AuthController@login');
+
+// just trying out 
